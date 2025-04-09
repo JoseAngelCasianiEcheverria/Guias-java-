ac08e4eaa5a8ffe0b753e4d4980fac5b547df496
chenaoh <cdhenao@gmail.com>
2025-04-09 15:15:51 -0500

    Subiendo guia 8

--- src/View/Guia6.java
+++ src/View/Guia6.java
@@ -44,7 +44,7 @@ public class Guia6 extends javax.swing.JFrame {
         });
 
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jButton1.setText("Pares");
+        jButton1.setText("Impares");
         jButton1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
