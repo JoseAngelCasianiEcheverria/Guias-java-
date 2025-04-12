6c65123a2b6fb4546ce24bf60769187b0e93fed8
chenaoh <cdhenao@gmail.com>
2025-04-11 21:49:54 -0500

    Subiendo guia 28

---
+++ src/View/Guia28.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Guia28 {
+    private double base;
+    private double altura;
+
+    public Guia28(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    public double getArea() {
+        return base * altura;
+    }
+
+    public double getPerimetro() {
+        return 2 * (base + altura);
+    }
+    
+    
+}
