7b398e884f9bef474920e9a16bfac2d6d32463bf
chenaoh <cdhenao@gmail.com>
2025-04-11 21:59:52 -0500

    Subiendo guia29

---
+++ src/View/Guia29.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Guia29 {
+
+    public class Rectangulo {
+        
+    private double base = 100;
+    private double altura = 50;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getArea() {
+        return base * altura;
+    }
+
+    public double getPerimetro() {
+        return 2 * (base + altura);
+    }
+
+    public void cuadrar() {
+        altura = base;
+    }
+}
+}
